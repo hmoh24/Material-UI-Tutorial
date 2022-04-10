2c0e677defca759a03ac10733248387846c63e98
hmoh24 <hamsam0097@gmail.com>
2022-04-10 15:35:29 +0100

    finished tutorial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Material UI Tutorial
+
+A small introduction to using Material UI and its concepts.
+Built a basic webpage with a few components.
+
+To run this project: 
+  - `npm i`
+  - `npm run start`
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button, Box } from '@mui/material';
+import { Typography, BottomNavigation, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button, Box } from '@mui/material';
 import { PhotoCamera } from '@mui/icons-material';
-import { makeStyles } from '@mui/material';
 
 const myStyle = {
   backgroundColor: 'background.paper',    //useful pattern if I want reusable style in many Box elements
-  marginTop: 20,
+  marginTop: 4,
   p: 2,
 }
 
 const pages = ['Products', 'Pricing', 'Blog'];
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const App = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
       <CssBaseline />
       <AppBar position='relative'>
         <Toolbar>
-          <PhotoCamera />
+          <PhotoCamera sx={{ mr: 2 }} />
           <Typography variant='h6'>
             Photo Album
           </Typography>
@@ -59,7 +59,38 @@ const App = () => {
             </div>
           </Container>
         </Box>
+        <Container maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map(() => (
+              <Grid item sx={{ padding: '20px 0' }} xs={12} sm={6} md={4}>
+                <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                  <CardMedia image='https://source.unsplash.com/random' title="Image title" sx={{ paddingTop: '56.25%' }} />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant='h5'>
+                      Heading
+                    </Typography>
+                    <Typography gutterBottom>
+                      This is a media card to describe the content
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color='primary'>View</Button>
+                    <Button size="small" color='primary'>Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
+      <BottomNavigation sx={{ backgroundColor: 'background.paper', py: '50px', px: '0', flexDirection: 'column', mt: 4 }}>
+        <Typography variant='h6' align='center' gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant='subtitle1' align='center' color="textSecondary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, aut.
+        </Typography>
+      </BottomNavigation>
     </>
   )
 }
